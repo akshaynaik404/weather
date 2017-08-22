d12f313c8f73ea659a3cb1cd3a7d756c489cd8c1
Akshay Naik <akshaynaik404@gmail.com>
2017-08-23 03:19:00 +0530

    Clean Project, create directory structure, and add hello world

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1 style={{textAlign: 'center'}}>Hello World!</h1>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'normalize.css';
-import App from './App';
+import { AppContainer } from './containers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
 registerServiceWorker();
