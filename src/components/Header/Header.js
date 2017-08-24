c13cb72a9c3393eb524d0bda199cf5f28ba0fbbf
Akshay Naik <akshaynaik404@gmail.com>
2017-08-24 08:49:00 +0530

    Add Header UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 import './Header.css';
+import { WeatherInput, WeatherSubmit } from '../';
 
 function Header() {
   return (
-    <header className="header">This is the Header</header>
+    <header className="header">
+      <h1 className="title">Weather</h1>
+      <section className="right">
+        <WeatherInput />
+        <WeatherSubmit />
+      </section>
+    </header>
   );
 }
 
